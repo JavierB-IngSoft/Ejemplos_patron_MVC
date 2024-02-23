45a1259dd0edb637718b56985b619f4ce84a2b4b
Albuguica <161070509+Albuguica@users.noreply.github.com>
2024-02-23 19:30:48 +0100

    Calculadora monolítica

---
+++ CalculadoraSuma/src/calculadorasuma/CalculadoraSuma.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadorasuma;
+
+/**
+ *
+ * @author Francisco Javier Bueno Guillén
+ * @date 18-2-2024
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class CalculadoraSuma extends JFrame implements ActionListener {
+    private JTextField numField1, numField2, resultField, voidField;
+    private JButton sumButton;
+
+    public CalculadoraSuma() {
+        setTitle("Calculadora de Suma monolítica");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 150);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4, 2));
+
+        panel.add(new JLabel("Número 1:"));
+        numField1 = new JTextField(10);
+        panel.add(numField1);
+
+        panel.add(new JLabel("Número 2:"));
+        numField2 = new JTextField(10);
+        panel.add(numField2);
+        
+
+        sumButton = new JButton("+");
+        sumButton.addActionListener(this);
+        panel.add(sumButton);
+        panel.add(new JLabel(""));
+        voidField = new JTextField(10);
+
+        
+        panel.add(new JLabel("Resultado:"));
+        resultField = new JTextField(10);
+        resultField.setEditable(false);
+        panel.add(resultField);
+
+        add(panel);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == sumButton) {
+            try {
+                double num1 = Double.parseDouble(numField1.getText());
+                double num2 = Double.parseDouble(numField2.getText());
+                double result = num1 + num2;
+                resultField.setText(String.valueOf(result));
+             
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(this, "Por favor ingrese números válidos.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new CalculadoraSuma();
+            }
+        });
+    }
+}
+
